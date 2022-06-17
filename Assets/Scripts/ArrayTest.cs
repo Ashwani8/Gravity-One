317f4615feec04fea5ad02f5acbee7e068e7422b
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-17 17:05:33 -0500

    add GameObject Arrays and Color type array

---
+++ Assets/Scripts/ArrayTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayTest : MonoBehaviour
+{
+    // create an array to hold 4 GameObject, so we need to create an array of type GameObject
+    public GameObject[] gameObjects = new GameObject[4];
+
+    // Arry of type Color for game objects
+    public Color[] colors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
