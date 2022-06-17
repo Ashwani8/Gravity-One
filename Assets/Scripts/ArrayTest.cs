12b83268478302df420a2d4c91a02ddfd31bec6c
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-17 17:27:37 -0500

    color the gamobjects in array

--- Assets/Scripts/ArrayTest.cs
+++ Assets/Scripts/ArrayTest.cs
@@ -12,7 +12,19 @@ public class ArrayTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Destroy(gameObjects[2],2f);
+        // color each game object with array index with the correspondeing array color index.
+        // we acces game objects with thier corresponding index
+        // > Getcomponent rendering properties > materils > assign corresponding color
+
+        gameObjects[0].GetComponent<Renderer>().material.color = colors[0];
+        // use ctrl +D to duplicate a line code
+        gameObjects[1].GetComponent<Renderer>().material.color = colors[1];
+        gameObjects[2].GetComponent<Renderer>().material.color = colors[2];
+        gameObjects[3].GetComponent<Renderer>().material.color = colors[3];
+        // if we don't know the length of the array we case use array.Length command
+        print(colors.Length);
+
     }
 
     // Update is called once per frame
