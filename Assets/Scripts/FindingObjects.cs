83fa04fedf2c60837b000e3dfe5829f88f0ccde3
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-20 10:50:50 -0500

    Assign and Find with Tag

--- Assets/Scripts/FindingObjects.cs
+++ Assets/Scripts/FindingObjects.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 // although these objects are not childeren of the empty Game object, it works.
 public class FindingObjects : MonoBehaviour
 {
-    GameObject searchObj, searchObj2;
+    GameObject searchObj, searchObj2, findWithTagObj;
     // Start is called before the first frame update
     void Start()
     {
-       searchObj= GameObject.Find("Sphere");
+       searchObj= GameObject.Find("Capsule");
         Destroy(searchObj);
         // searching a child object in heriarchy
         searchObj2 = GameObject.Find("Cube/Cube2/Enemy");
         Destroy(searchObj2, 2f);
+        // searching with tag
+        findWithTagObj = GameObject.FindGameObjectWithTag("Ball");
+        Destroy(findWithTagObj, 3f);
     }
 
     // Update is called once per frame
