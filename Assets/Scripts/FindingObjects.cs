1e706bde66bad48cdc61a01e1ad9613ed7db0f86
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-20 11:12:54 -0500

    Find multiple game objects with same tag and destroy them

--- Assets/Scripts/FindingObjects.cs
+++ Assets/Scripts/FindingObjects.cs
@@ -7,17 +7,34 @@ using UnityEngine;
 public class FindingObjects : MonoBehaviour
 {
     GameObject searchObj, searchObj2, findWithTagObj;
+
+    // create gameobject array to store multiple game objects with same tag
+    GameObject[] enemies;
+
     // Start is called before the first frame update
     void Start()
     {
-       searchObj= GameObject.Find("Capsule");
-        Destroy(searchObj);
+        //  searchObj= GameObject.Find("Capsule");
+        //  Destroy(searchObj);
         // searching a child object in heriarchy
-        searchObj2 = GameObject.Find("Cube/Cube2/Enemy");
-        Destroy(searchObj2, 2f);
+        //  searchObj2 = GameObject.Find("Cube/Cube2/Enemy");
+        //  Destroy(searchObj2, 2f);
         // searching with tag
-        findWithTagObj = GameObject.FindGameObjectWithTag("Ball");
-        Destroy(findWithTagObj, 3f);
+        //  findWithTagObj = GameObject.FindGameObjectWithTag("Ball");
+        //  Destroy(findWithTagObj, 3f);
+
+        // Find multiple Game objects with same Tag, store them in an array, and then pick up
+        // one by one to destroy them,
+
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        // we create a new game object enemy to sort out enemies array and destroy objects in foreach loop
+        foreach(GameObject enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+
+
     }
 
     // Update is called once per frame
