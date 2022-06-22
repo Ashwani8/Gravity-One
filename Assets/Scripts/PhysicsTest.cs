e60458e84ea22360e70019c34de0c142def8eeae
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-22 08:34:38 -0500

    add jump with space bar

--- Assets/Scripts/PhysicsTest.cs
+++ Assets/Scripts/PhysicsTest.cs
@@ -6,7 +6,8 @@ public class PhysicsTest : MonoBehaviour
 {
     Rigidbody rb;// get access to rigid body
     float xInput, zInput;
-    public float speed;
+    public float speed, jumpForce;
+    bool jump = false;
     // it is good idea to use awake method rathar than start for position. similarly
     // it is good idea to use FixedUpdate for Physics activities rather than regular update
     // method as the later depends on computer speed
@@ -26,8 +27,14 @@ public class PhysicsTest : MonoBehaviour
     {
         xInput = Input.GetAxis("Horizontal"); // move smoothly and give us x value when we press left arrow key
         zInput = Input.GetAxis("Vertical"); // move smoothly FWD and give us y value when we press right arrow key
-        print(xInput + zInput);
-    
+                                            //print(xInput + zInput);
+
+        // check is the space bar is pressed down == "Jump" key word input axis
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            
+        }
     
     }
 
@@ -36,5 +43,17 @@ public class PhysicsTest : MonoBehaviour
     {
         rb.velocity = new Vector3(xInput*speed,rb.velocity.y , zInput*speed); // note the velocity in y axis
         // is controlled by gravity, so we are just reading whatever it is rb.velocity.y
+
+        // if jump is true when it will call Jump() 
+        if (jump == true)
+        {
+            Jump(); // call jump function when space bar is pressed
+            jump = false;
+        }
+    }
+   
+    void Jump()
+    {// add force in y 
+        rb.AddForce(0, jumpForce, 0); // make sure to set jumpForce value >100f
     }
 }
