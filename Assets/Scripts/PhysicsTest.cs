d31a61c382c4e19367bfdc486c3489eaf48ae569
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-21 20:21:04 -0500

    give velocity to game object with rigid body

--- Assets/Scripts/PhysicsTest.cs
+++ Assets/Scripts/PhysicsTest.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PhysicsTest : MonoBehaviour
 {
+    Rigidbody rb;// get access to rigid body
+    float xInput, yInput;
+    public float speed;
+    // it is good idea to use awake method rathar than start for position. similarly
+    // it is good idea to use FixedUpdate for Physics activities rather than regular update
+    // method as the later depends on computer speed
     // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>(); // get access when app awake
+    }
     void Start()
     {
-        
+        // move game object with rigid body in forward (z-) direction with the given velocity
+        rb.velocity = new Vector3(0, 0, speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
