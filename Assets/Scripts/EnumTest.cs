2aee845f014383f168bc5ea3192d3f39e94b0809
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-28 16:48:29 -0500

    Access Enum with index or assigned int

--- Assets/Scripts/EnumTest.cs
+++ Assets/Scripts/EnumTest.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnumTest : MonoBehaviour
 {
     // Enum allow us to create new type of value or data type
-   public enum GameState { Ready, Playing, Pause, GameOver}
+   public enum GameState { Ready, Playing, Pause, GameOver = 6}
    public GameState state;
     // Start is called before the first frame update
     void Start()
@@ -19,16 +19,20 @@ public class EnumTest : MonoBehaviour
         switch(state)
         {
             case GameState.Ready:
-                print("You are ready");
+                // print("You are ready");
+                print(GameState.Ready); // will print Ready
                 break;
             case GameState.Playing:
                 print("Game On");
+                print((int)GameState.Playing); // will display the index number of the Enum
                 break;
             case GameState.Pause:
                 print("Pause");
                 break;
             case GameState.GameOver:
                 print("GameOver");
+                // will dipaly the assigned Enum
+                print((int)GameState.GameOver);
                 break;
         }       
     }
