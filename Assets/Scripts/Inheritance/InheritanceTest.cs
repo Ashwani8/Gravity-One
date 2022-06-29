9275d6c36f80697867096857125597501d21a007
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-29 11:43:40 -0500

    learn Inheritance

---
+++ Assets/Scripts/Inheritance/InheritanceTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InheritanceTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy enemy = new Enemy();
+        enemy.Attack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
