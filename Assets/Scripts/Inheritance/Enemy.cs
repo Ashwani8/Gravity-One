f0ebca30e5bf7ed2ef0da6d7fbfc31d6c5b828ac
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-06-29 16:07:37 -0500

    access inheritance

--- Assets/Scripts/Inheritance/Enemy.cs
+++ Assets/Scripts/Inheritance/Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Enemy 
 {
+ // if we make any funtion private we won't be able to access the function.
     public void Attack()
     {
         Debug.Log("Enemy Attack");
--- Assets/Scripts/Inheritance/InheritanceTest.cs
+++ Assets/Scripts/Inheritance/InheritanceTest.cs
@@ -7,8 +7,12 @@ public class InheritanceTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Enemy enemy = new Enemy();
-        enemy.Attack();
+        // Enemy enemy = new Enemy();
+        // enemy.Attack();
+        
+        // Dragon inherited Enemy, so it will produce the same results.
+        Dragon dragon = new Dragon();
+        dragon.Attack();
     }
 
     // Update is called once per frame
