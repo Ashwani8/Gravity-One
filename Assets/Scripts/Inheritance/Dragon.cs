0be165c366daf2270e08e346d0cc39d2d9641c6b
Ashwani Kumar <ashwani.oct8@gmail.com>
2022-07-01 10:24:49 -0500

    Polymorphism not clear some error

--- Assets/Scripts/Inheritance/Dragon.cs
+++ Assets/Scripts/Inheritance/Dragon.cs
@@ -7,10 +7,37 @@ public class Dragon : MonoBehaviour
     private void Start()
     {
         Dragon dragon = new Dragon();
-        dragon.Attack(); 
+        dragon.Attack();
+
+        // create new instant of blue dragon
+        Dragon dragonB = new BlueDragon(); // we can do this since BlueDragon inderits from Dragon
+                dragonB.Attack(); // this will call blue dragon
+
+        Dragon dragonR = new RedDragon();
+        dragonR.Attack();
     }
-    public void Attack()
+    public virtual void Attack()
     {
         print("Dragon Attack");
     }
 } // Dragon class ends here
+
+// we plan to creat multiple classes here which also inherited Dragon class and thus monobehaviour.
+
+public class BlueDragon: Dragon
+    {
+    public override void Attack()
+    { // since we are using the same name of the function, Attack, it will hide the function from base class Dragon.
+       
+        print(" Blue Dragon Attack");
+    }
+    
+    } // Blue Dragon ends class ends here
+
+public class RedDragon: Dragon
+{
+    public override void Attack()
+    {
+        print("Red Dragon attack");
+    }
+}
